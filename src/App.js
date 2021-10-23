a509fca5c1b111699ce5736f13d88cdf5b6675b9
vinyl704 <mpbarber@mymail.gaston.edu>
2021-10-22 21:44:05 -0400

    added labels to inputs, added distance from each node

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React,{ useState, useEffect } from 'react'
 import Rewards from './components/Rewards';
+import DistanceBetween from './components/DistanceBetween'
 
 function App() {
   const [lat, setLat] = useState([]);
@@ -17,11 +18,12 @@ function App() {
        setLat(position.coords.latitude);
        setLong(position.coords.longitude);
       });
+      if(lat !=undefined && long != undefined &&distance){
       await fetch(`https://api.helium.io/v1/hotspots/location/distance?lat=${lat}&lon=${long}&distance=${dist}`, {mode:'cors'})
         .then(res=>res.json())
           .then(result=>{
             setData(result.data);  
-          })
+          })}
       }
     fetchData();
   }
@@ -30,17 +32,18 @@ function App() {
   let spots = data.map(a=>
             (
               <ul className="spots" style={{listStyleType:'none',display:'flex',flexDirection:'column',margin:20}}>
-              
+              {console.log(lat,long)}
                 <li><strong>{a.name}</strong></li>
                 <li className="addy">{a.address}</li>
                 
                 <li>Street Name: {a.geocode.long_street}</li>
                 <li>City: {a.geocode.long_city}</li>
                 <li>Reward Scale: {a.reward_scale}</li> 
-              
+                <DistanceBetween oLat={lat} oLong={long} destLat = {a.lat} destLong = {a.lng}/>
                 <Rewards a={a.address} timeFrame={timeFrame}/>
             </ul>)
             );
+          
   
 const handleChange=(event)=>{
   setDist(event.target.value) 
@@ -57,12 +60,14 @@ const handleRange=(event)=>{
        Hotspots near you
      </h1>
      <p>There are <span className="distance">{spots.length}</span> Helium Hotspots within <br/>{dist} meters of your location</p>
-     <input type='number' placeholder='10000' id='distance' onChange={handleChange}/>
+     <input name="distance" type='number' placeholder='10000' id='distance' onChange={handleChange}/> 
+     <label id="distLabel" for="distance">Meters</label>
      <select name="timeFrame" id="timeFrame" onChange={handleRange}>
        <option value='1'>1 Day</option>
-       <option value="7">1 Week</option>
-       <option value="30">1 Month</option>
+       <option value="7">7 Days</option>
+       <option value="30">30 Days</option>
      </select>
+     <label id="timeLabel" for="timeFrame">Rewards Duration</label>
 
      <ul>
       {spots}
